254f19648fbe32194770f84666a29d5cc752d783
Lucas Oliveira <lucasop888@yahoo.com>
2021-07-19 15:08:44 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./assets/mahira01.png";
+
+import fire from "./assets/Element_Fire.png";
+import water from "./assets/Element_Water.png";
+import earth from "./assets/Element_Earth.png";
+import wind from "./assets/Element_Wind.png";
+import light from "./assets/Element_Light.png";
+import dark from "./assets/Element_Dark.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <body>
+          <div class="tab">
+            <ul class="tabnav">
+              <li class="tabactive fire">
+                <img
+                  src={fire}
+                  alt="Fire"
+                  style={{ width: "auto", height: "auto" }}
+                />
+              </li>
+
+              <li class="tabactive water">
+                <img
+                  src={water}
+                  alt="Water"
+                  style={{ width: "auto", height: "auto" }}
+                />
+              </li>
+
+              <li class="tabactive earth">
+                <img
+                  src={earth}
+                  alt="Earth"
+                  style={{ width: "auto", height: "auto" }}
+                />
+              </li>
+
+              <li class="tabactive wind">
+                <img
+                  src={wind}
+                  alt="Wind"
+                  style={{ width: "auto", height: "auto" }}
+                />
+              </li>
+
+              <li class="tabactive light">
+                <img
+                  src={light}
+                  alt="Light"
+                  style={{ width: "auto", height: "auto" }}
+                />
+              </li>
+
+              <li class="tabactive dark">
+                <img
+                  src={dark}
+                  alt="Dark"
+                  style={{ width: "auto", height: "auto" }}
+                />
+              </li>
+            </ul>
+          </div>
+          <div class="card">
+            <img src={logo} alt="Avatar" style={{ width: "auto" }} />
+            <div class="container">
+              <h4>
+                <b>Mahira</b>
+              </h4>
+              <p>
+                Um dos Doze Generais Divinos, Mahira é a Rainha dos Galos.
+                Embora ela não tenha as asas adequadas a esse título, sua
+                pequena estrutura abriga grande poder e gênio criativo. Algum
+                dia ela conduzirá o povo do céu por todo o caminho até os céus.
+              </p>
+            </div>
+          </div>
+        </body>
       </header>
     </div>
   );
